560de1c528a4fbb812f193ae0d38029bbe819ddf
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-06-19 18:09:06 +0900

    Basic react-redux setup

--- src/index.js
+++ src/index.js
@@ -1,60 +1,13 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 
-const plus = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1
-    default :
-      return count
-  }
-
-  // if (action.type === "ADD") {
-  //   return count + 1;
-  // } else if (action.type === "MINUS") {
-  //   return count - 1;
-  // } else {
-  //   return count;
-  // }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-}
-
-countStore.subscribe(onChange);
-
-plus.addEventListener("click", () => countStore.dispatch({ type: ADD }));
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
-
-
-
-// <<< The old way >>>
-
-// let count = 0;
-// number.innetText = count;
-
-// const updateText = () => {
-//   number.innerText = count;
-// }
-
-// const handleAdd = () => {
-//   count = count + 1;
-//   updateText();
-// }
-
-// const handleMinus = () => {
-//   count = count - 1;
-//   updateText();
-// // }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
